9ffe979a1c5138d1eaaff6766e8227458e6003dc
SebastianGanoza73 <sebastian.ganoza73@gmail.com>
2026-08-05 23:41:21 -0500

    Wrap App component with ModeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { ModeProvider } from './context/ModeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <AuthProvider>
-          <App />
+          <ModeProvider>
+            <App />
+          </ModeProvider>
         </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
